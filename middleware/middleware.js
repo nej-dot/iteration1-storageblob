a225a6a323c1c57620ab03ae5eb5e604fde04a0b
nej-dot <ha6446ha-s@student.lu.se>
2024-04-09 17:55:10 +0200

    moved middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,12 +1,11 @@
-// pages/_middleware.js
+// middleware/middleware.js
 
 import { NextResponse } from 'next/server';
 
-export default function middleware(request) {
-    const { headers } = request;
-    const auth = headers.get('authorization');
+export function middleware(req) {
+    const basicAuth = req.headers.get('authorization');
 
-    if (!auth) {
+    if (!basicAuth) {
         return new Response('Authorization required', {
             status: 401,
             headers: {
@@ -15,7 +14,7 @@ export default function middleware(request) {
         });
     }
 
-    const encodedCredentials = auth.split(' ')[1];
+    const encodedCredentials = basicAuth.split(' ')[1];
     const [username, password] = Buffer.from(encodedCredentials, 'base64').toString().split(':');
     const expectedPassword = process.env.PASSWORD_PROTECT;
 
