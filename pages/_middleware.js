f0b8f079b324a1f815c160d60735a25238e91530
nej-dot <ha6446ha-s@student.lu.se>
2024-04-09 17:48:25 +0200

    fix middlewarew

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,25 +2,26 @@
 
 import { NextResponse } from 'next/server';
 
-export function middleware(request) {
-  const basicAuth = request.headers.get('authorization');
+export default function middleware(request) {
+    const { headers } = request;
+    const auth = headers.get('authorization');
 
-  if (!basicAuth) {
-    return new Response('Authorization required', {
-      status: 401,
-      headers: {
-        'WWW-Authenticate': 'Basic realm="Secure Area"',
-      },
-    });
-  }
+    if (!auth) {
+        return new Response('Authorization required', {
+            status: 401,
+            headers: {
+                'WWW-Authenticate': 'Basic realm="Secure Area"',
+            },
+        });
+    }
 
-  const auth = basicAuth.split(' ')[1];
-  const [username, password] = Buffer.from(auth, 'base64').toString().split(':');
-  const expectedPassword = process.env.PASSWORD_PROTECT;
+    const encodedCredentials = auth.split(' ')[1];
+    const [username, password] = Buffer.from(encodedCredentials, 'base64').toString().split(':');
+    const expectedPassword = process.env.PASSWORD_PROTECT;
 
-  if (password !== expectedPassword) {
-    return new Response('Unauthorized', { status: 401 });
-  }
+    if (password !== expectedPassword) {
+        return new Response('Unauthorized', { status: 401 });
+    }
 
-  return NextResponse.next();
+    return NextResponse.next();
 }
